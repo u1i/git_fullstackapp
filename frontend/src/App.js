7dfa7b059903e93ff050befdd946b5c0c43697b0
wzjustuslim <70901244+wzjustuslim@users.noreply.github.com>
2020-10-01 22:29:00 +0800

    modify frontend app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,25 +2,34 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { serverResponse: ""};
+  }
+
+  callAPIServer() {
+    fetch("http://localhost:7000/mypage")
+      .then(res => res.text())
+      .then(res => this.setState({ serverResponse:res }))
+      .catch(err => err);
+  }
+
+  componentDidMount() {
+    this.callAPIServer();
+  }
+
+  render() {
+    return(
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo"/>
+          <h1 className="App-title">Welcome to FinTechSG React Course</h1>
+          <h2 className="App-intro">{this.state.serverResponse}</h2>
+        </header>
+      </div>
+    )
+  }
 }
 
 export default App;
